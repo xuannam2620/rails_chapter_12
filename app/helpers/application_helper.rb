7da46600ae16fc509092828ea5e31565eecf5e5b
Tran Xuan Nam <tran.xuan.nam@framgia.com>
2017-02-14 12:40:16 +0700

    Finish static pages with Advanced testing and I18n

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = "")
+    if page_title.empty?
+      ""
+    else
+      page_title + " | "
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "static_pages/contact"
+
+  get "static_pages/about"
+
+  get "static_pages/home"
+
+  get "static_pages/help"
+
+  root "static_pages#home"
 end
