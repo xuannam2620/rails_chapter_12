1a07abf9f2e0ffbce82a2f5789f25c56c0283f7a
Tran Xuan Nam <tran.xuan.nam@framgia.com>
2017-02-15 07:41:38 +0700

    chapter-6

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
+  
+  get "/contact", to: "static_pages#contact"
 
-  get "static_pages/contact"
+  get "/about", to: "static_pages#about"
 
-  get "static_pages/about"
+  get "/help", to: "static_pages#help"
 
-  get "static_pages/home"
-
-  get "static_pages/help"
+  get "/signup", to: "users#new"
 
   root "static_pages#home"
 end
