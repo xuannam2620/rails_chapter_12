83bb6571b8716f89481288d77a3a4a3c5ca86c8b
Tran Xuan Nam <tran.xuan.nam@framgia.com>
2017-02-15 08:56:27 +0700

    chapter 4

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,6 @@
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
+ENV["RAILS_ENV"] ||= "test"
+require File.expand_path("../../config/environment", __FILE__)
+require "rails/test_help"
 require "minitest/reporters"
 Minitest::Reporters.use!
 
