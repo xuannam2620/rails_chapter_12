9da2b8b3ba6fbd76fcf8f8e351242935764d2b09
Tran Xuan Nam <tran.xuan.nam@framgia.com>
2017-02-15 17:13:23 +0700

    chapter 7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def show
+    @user = User.find_by id: params[:id]
+    @user || render(file: "public/404.html", status: 404, layout: true)
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t :welcome
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit :name, :email, :password,
+        :password_confirmation
+    end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  def gavatar_for user
+    gavatar_id = Digest::MD5::hexdigest user.email.downcase
+    gavatar_url = "https://secure.gravatar.com/avatar/#{gavatar_id}"
+    image_tag(gavatar_url, alt: user.name, class: "gavatar")
+  end
 end
